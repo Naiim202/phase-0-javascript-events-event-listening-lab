80f042dcfbd7df98a219cd253e94c445e80e8d6c
hani <hani@DESKTOP-5I66P1U.localdomain>
2024-07-11 14:36:43 +0300

    events-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+    if (input) {
+      input.addEventListener('click', function() {
+        console.log('Button was clicked!');
+      });
+    }
+  }
+  
+  module.exports = addingEventListener;
+  
